c9d217fc3b373754c51892af62a69fac105c0d96
Ogi Bin Edi <ogibinedi@gmail.com>
2022-08-24 20:30:30 +0700

    Menghapus komentar yang tidak perlu

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/custom.css';
-// import PersonalNotesApp from './components/PersonalNotesApp';
-// import './styles/style.css';
 import PersonalNoteApp from './components/PersonalNoteApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
